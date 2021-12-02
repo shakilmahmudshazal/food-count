3debc0157da473b589db79e10334de8ab93ff099
Md. Shakil Mahmud <mdshakilmahmud517@gmail.com>
2021-12-02 20:22:11 +0600

    added to firebase 2

---
+++ build/precache-manifest.43daf6abc13cbea9611d23006cd289bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79fec50e3eea6a3512bbd6ac2570372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "823d0b935697139bdaa5",
+    "url": "/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "6ac57a079a5452575991",
+    "url": "/static/css/main.94d6e5c0.chunk.css"
+  },
+  {
+    "revision": "823d0b935697139bdaa5",
+    "url": "/static/js/2.d7e236f4.chunk.js"
+  },
+  {
+    "revision": "6ac57a079a5452575991",
+    "url": "/static/js/main.32eb3220.chunk.js"
+  },
+  {
+    "revision": "09dab452e172c9cadbb9",
+    "url": "/static/js/runtime-main.f9557126.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
